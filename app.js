5564b6134945e155ddb014e8065fe02c51f62600
davecharm16 <45230660+davecharm16@users.noreply.github.com>
2020-11-09 11:55:37 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const text = "Hello, I am Dave Charm";
 const nav_button = document.querySelectorAll(".nav-burger");
 const drop_down = document.querySelector(".drop_down");
 let i = 0;
-
+const project_image = document.querySelectorAll(".container-project");
 
 var arr = text.split("");
 arr.forEach(letters => {
@@ -48,7 +48,9 @@ function complete() {
 
 nav_button[0].addEventListener("click", drop_close);
 nav_button[1].addEventListener("click", drop);
-
+project_image.forEach(element => {
+    element.addEventListener('click', carousel);
+});
 
 console.log(nav_button);
 /*nav -funct */
@@ -63,4 +65,31 @@ function drop_close() {
     nav_button[0].classList.add("close");
     nav_button[1].classList.remove("close");
     drop_down.classList.add("close")
+}
+
+
+// function rotate_and_pop() {
+//     const image = this.firstElementChild.firstElementChild;
+//     const div_image = this.firstElementChild;
+//     image.style.opacity = '0.99';
+//     image.style.transform = 'scale(0.5)';
+//     div_image.style.transition = "0.5s all ease";
+//     // this.firstElementChild.style.display = "none";
+//     // this.lastElementChild.style.display = "block";
+
+
+// }
+
+//test carousel//
+
+
+
+function carousel() {
+    // const image = this.firstElementChild.firstElementChild;
+    const div_image = this.firstElementChild;
+    div_image.style.transform = '0.5 all ease';
+    div_image.firstElementChild.style.transform = 'translateX(-110%)';
+    div_image.firstElementChild.style.transition = '1s all ease-in-out';
+    div_image.lastElementChild.style.transition = '2s all ease-in-out';
+    div_image.lastElementChild.style.opacity = 0.99;
 }
